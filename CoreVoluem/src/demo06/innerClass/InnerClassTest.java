4cc95bdfe6d0c667d18ee8426db92969eceaad3c
hx <913415840@qq.com>
2023-03-20 22:50:29 +0800

    新增类
    Signed-off-by: hx <913415840@qq.com>

---
+++ CoreVoluem/src/demo06/innerClass/InnerClassTest.java
@@ -0,0 +1,49 @@
+package demo06.innerClass;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.Instant;
+
+
+/**
+ * @author 烂醉花间
+ * @date 2023/3/20  22:21
+ */
+public class InnerClassTest {
+    public static void main(String[] args) {
+        TalkingClock talkingClock = new TalkingClock(1000, true);
+        talkingClock.start();
+
+        JOptionPane.showMessageDialog(null,"Quit program");
+        System.exit(0);
+    }
+}
+
+class TalkingClock{
+    private int interval;
+    private boolean beep;
+
+    public TalkingClock(int interval, boolean beep)  {
+        this.interval = interval;
+        this.beep = beep;
+    }
+
+    public void start(){
+        TimerPrinter timerPrinter = new TimerPrinter();
+        Timer timer = new Timer(interval, timerPrinter);
+        timer.start();
+    }
+
+    public class TimerPrinter implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("At the tone,the time is"+ Instant.ofEpochMilli(e.getWhen()));
+            if (beep) {
+                Toolkit.getDefaultToolkit().beep();
+            }
+        }
+    }
+
+}
