d50f2ef54b5722986484a8936cecdfd4103d9056
hx <913415840@qq.com>
2023-03-25 19:53:16 +0800

    新增类
    Signed-off-by: hx <913415840@qq.com>

---
+++ CoreVoluem/src/demo07/logging/LoggingImageViewer.java
@@ -0,0 +1,146 @@
+package demo07.logging;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.logging.*;
+
+/**
+ * @author 烂醉花间
+ * @date 2023/3/25  19:03
+ */
+public class LoggingImageViewer {
+    public static void main(String[] args) {
+        if (System.getProperty("java.util.logging.config.class") == null
+                && System.getProperty("java.util.logging.config.file") == null) {
+            try {
+                Logger.getLogger("core_Java_volume.CoreVolume").setLevel(Level.ALL);
+                final int LOG_ROTATION_COUNT = 10;
+                FileHandler handler = new FileHandler("%h/LoggingImageViewer.log", 0, LOG_ROTATION_COUNT);
+                Logger.getLogger("core_Java_volume.CoreVolume").addHandler(handler);
+            } catch (IOException e) {
+                Logger.getLogger("core_Java_volume.CoreVolume").log(Level.SEVERE,"Can't crete log file handler",e);
+            }
+        }
+
+        EventQueue.invokeLater(() -> {
+            WindowHandler windowHandler = new WindowHandler();
+            windowHandler.setLevel(Level.ALL);
+            Logger.getLogger("core_Java_volume.CoreVolume").addHandler(windowHandler);
+
+            ImageViewerFrame frame = new ImageViewerFrame();
+            frame.setTitle("LoggingImageViewer");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            Logger.getLogger("core_Java_volume.CoreVolume").fine("Showing frame");
+            frame.setVisible(true);
+        });
+
+    }
+}
+
+class ImageViewerFrame extends JFrame {
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 400;
+
+    private JLabel jLabel;
+    private static Logger logger = Logger.getLogger("core_Java_volume.CoreVolume");
+
+    public ImageViewerFrame() {
+        logger.entering("ImageViewerFrame", "<init>");
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu menu = new JMenu("File");
+        menuBar.add(menu);
+
+        JMenuItem openItem = new JMenuItem("Open");
+        menu.add(openItem);
+
+        openItem.addActionListener(new FileOpenListener());
+
+        JMenuItem exitItem = new JMenuItem("Exit");
+        menu.add(exitItem);
+        exitItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                logger.fine("Exiting.");
+                System.exit(0);
+            }
+        });
+        jLabel = new JLabel();
+        add(jLabel);
+        logger.exiting("ImageViewerFrame", "<init>");
+    }
+
+    private class FileOpenListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            logger.entering("ImageViewerFrame.FileOpenListener", "actionPerformed", e);
+
+            JFileChooser chooser = new JFileChooser();
+            chooser.setCurrentDirectory(new File("."));
+
+            chooser.setFileFilter(new FileFilter() {
+                @Override
+                public boolean accept(File f) {
+                    return f.getName().toLowerCase().endsWith(".gif") || f.isDirectory();
+                }
+
+                @Override
+                public String getDescription() {
+                    return "GIF Images";
+                }
+            });
+
+            int r = chooser.showOpenDialog(ImageViewerFrame.this);
+
+            if (r == JFileChooser.APPROVE_OPTION) {
+                String name = chooser.getSelectedFile().getPath();
+                logger.log(Level.FINE, "Reading file {0}", name);
+                jLabel.setIcon(new ImageIcon(name));
+            } else {
+                logger.fine("File open dialog canceled");
+            }
+            logger.exiting("ImageViewerFrame.FilerOpenListener", "actionPerformed");
+        }
+    }
+}
+
+class WindowHandler extends StreamHandler {
+    private JFrame frame;
+
+    public WindowHandler() {
+        frame = new JFrame();
+        JTextArea output = new JTextArea();
+        output.setEditable(false);
+        frame.setSize(200, 200);
+        frame.add(new JScrollPane(output));
+        frame.setFocusableWindowState(false);
+        frame.setVisible(true);
+        setOutputStream(new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {
+
+            }
+
+            public void write(byte[] b, int off, int len) {
+                output.append(new String(b, off, len));
+            }
+        });
+    }
+
+    public void publish(LogRecord record) {
+        if (!frame.isVisible()) return;
+        super.publish(record);
+        flush();
+    }
+}
