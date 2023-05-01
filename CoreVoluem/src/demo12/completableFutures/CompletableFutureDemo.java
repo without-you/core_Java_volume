8e7716b00ca86aa4ef1175bf8830309c70a9506a
hx <913415840@qq.com>
2023-05-01 19:01:02 +0800

    新增类
    Signed-off-by: hx <913415840@qq.com>

---
+++ CoreVoluem/src/demo12/completableFutures/CompletableFutureDemo.java
@@ -0,0 +1,100 @@
+package demo12.completableFutures;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author 烂醉花间
+ * @date 2023/5/1  18:27
+ * 读取一个web网页，扫描网页得到其中的图像，并且保存在本地
+ */
+public class CompletableFutureDemo {
+    private static final Pattern IMG_PATTERN = Pattern
+            .compile("[<]\\s*[iI][mM][gG]\\s*[^>]*[sS][rR][cC]\\s*[=]\\s*['\"]([^'\"]*)['\"][^>]*[>]");
+    private ExecutorService executor = Executors.newFixedThreadPool(8);
+    private URL urlToProcess;
+
+    public CompletableFuture<String> readPage(URL url) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                var contents = new String(url.openStream().readAllBytes(), StandardCharsets.UTF_8);
+                System.out.println("Read page from " + url);
+                return contents;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }, executor);
+    }
+
+    public List<URL> getImageURLs(String webpage) {
+        try {
+            var result = new ArrayList<URL>();
+            Matcher matcher = IMG_PATTERN.matcher(webpage);
+            while (matcher.find()) {
+                var url = new URL(urlToProcess, matcher.group(1));
+                result.add(url);
+            }
+            System.out.println("Found URLs: " + result);
+            return result;
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public CompletableFuture<List<BufferedImage>> getImages(List<URL> urls) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                var result = new ArrayList<BufferedImage>();
+                for (URL url : urls) {
+                    result.add(ImageIO.read(url));
+                    System.out.println("Loaded " + url);
+                }
+                return result;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }, executor);
+    }
+
+    public void saveImages(List<BufferedImage> images) {
+        System.out.println("Saving " + images.size() + " images");
+        try {
+            for (int i = 0; i < images.size(); i++) {
+                String filename = "/tmp/image" + (i + 1) + ".png";
+                ImageIO.write(images.get(i), "PNG", new File(filename));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        executor.shutdown();
+    }
+
+    public void run(URL url) {
+        urlToProcess = url;
+        CompletableFuture.completedFuture(url)
+                .thenComposeAsync(this::readPage, executor)
+                .thenApply(this::getImageURLs)
+                .thenCompose(this::getImages)
+                .thenAccept(this::saveImages);
+    }
+
+    public static void main(String[] args) {
+        try {
+            new CompletableFutureDemo().run(new URL("http://horstmann.com/index.html"));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ CoreVoluem/src/demo12/concurrentHashMap/CHMDemo.java
@@ -0,0 +1,58 @@
+package demo12.concurrentHashMap;
+
+import javax.swing.plaf.PanelUI;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author 烂醉花间
+ * @date 2023/5/1  15:04
+ * 统计一个目录树下的Java文件中的所有单词
+ */
+public class CHMDemo {
+    public static ConcurrentHashMap<String, Long> map = new ConcurrentHashMap<>();
+
+    public static void process(Path file) {
+        try (var in = new Scanner(file)) {
+            while (in.hasNext()) {
+                String word = in.next();
+                map.merge(word, 1L, Long::sum);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Set<Path> descendants(Path rootDir) throws IOException {
+        try (Stream<Path> entries = Files.walk(rootDir)) {
+            return entries.collect(Collectors.toSet());
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int processors = Runtime.getRuntime().availableProcessors();
+        ExecutorService executorService = Executors.newFixedThreadPool(processors);
+        Path pathToRoot = Path.of("");
+        for (Path path : descendants(pathToRoot)) {
+            if (path.getFileName().toString().endsWith(".java")) {
+                executorService.execute(() -> process(path));
+            }
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.MINUTES);
+        map.forEach((k, v) -> {
+            if (v >= 10) {
+                System.out.println(k + " occurs " + v + " times");
+            }
+        });
+    }
+}
